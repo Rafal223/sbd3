550959f13488c57bf72377217bc4ac05eb04cd83
Rafal223 <113582561+Rafal223@users.noreply.github.com>
2023-01-04 10:14:54 +0100

    Add files via upload

--- zapytania.sql
+++ zapytania.sql
@@ -29,3 +29,24 @@ SELECT COUNT(orders.id) FROM customers JOIN orders ON customers.id=orders.custom
 SELECT * FROM customers JOIN orders ON customers.id=orders.customer_id;
 SELECT * FROM employees RIGHT JOIN orders ON employees.id=orders.employee_id;
 SELECT * FROM customers AS cus1 JOIN customers AS cus2 ON cus1.city=cus2.city AND cus1.customer_name!=cus2.customer_name;
+
+SELECT * FROM orders ORDER BY id DESC LIMIT 10;
+SELECT * FROM orders LIMIT 10 OFFSET 10;
+
+SELECT country FROM customers HAVING Count(country)>5;
+SELECT customer_name FROM customers JOIN orders WHERE customers.id=orders.customer_id HAVING Count(orders.customer_id)>2 AND Count(orders.customer_id)<4 ORDER BY customer_name DESC;
+SELECT customers.id,customer_name FROM customers JOIN orders WHERE customers.id=orders.customer_id HAVING Count(orders.customer_id)>10;
+SELECT last_name FROM employees JOIN orders WHERE employees.id=0 AND employees.id=1 HAVING Count(orders.employee_id)>25;
+SELECT name FROM suppliers WHERE EXISTS (SELECT * FROM products WHERE suppliers.id=products.supplier_id AND products.price < 20);
+SELECT name FROM suppliers WHERE EXISTS (SELECT * FROM products WHERE suppliers.id=products.supplier_id AND products.price = 22);
+
+SELECT name FROM products WHERE ANY (SELECT * FROM orders_details WHERE  HAVING Count(orders_details.product_id)=10);
+SELECT
+SELECT
+SELECT
+
+SELECT name CASE 
+WHEN orders_details.quantity>30 THEN 'Liczba sztuk jest większa niż 30'
+WHEN orders_details.quantity=30 THEN 'Liczba sztuk jest równa 30'
+ELSE 'Liczba sztuk jest mniejsza niż 30'
+END AS Liczba_Sztuk
